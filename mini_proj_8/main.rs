456b60230932cac01ebdb35a299e1b3b81de0ec2
VCM admin account <vcm@vcm-30918.vm.duke.edu>
2023-10-22 23:04:43 -0400

    upload runtime result

--- mini_proj_8/main.rs
+++ mini_proj_8/main.rs
@@ -1,7 +1,7 @@
 use mini_proj_8::{connect_db, create_tables_if_not_exists, clear_table, insert_data_from_csv, complex_query};
 
 #[tokio::main]
-fn main() {
+async fn main() {
     let pool = connect_db();
     create_tables_if_not_exists(&pool).await;
     clear_table(&pool).await;
